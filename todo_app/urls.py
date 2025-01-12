11c5a5a5d22544550cb58c31bd2295b6be485d0c
najasinis <tttjjjyyy1234@daum.net>
2025-01-13 01:53:44 +0900

    update file

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from todo.views import todo_list, add_todo, edit_todo, delete_todo
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', todo_list, name='todo_list'),
     path('add/', add_todo, name='add_todo'),
     path('edit/<int:todo_id>/', edit_todo, name='edit_todo'),
     path('delete/<int:todo_id>/', delete_todo, name='delete_todo'),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 ]
