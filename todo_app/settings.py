854050c7bf9bbf85744b50df74c4cc567db92bf7
najasinis <tttjjjyyy1234@daum.net>
2025-01-28 16:54:07 +0900

    update file

--- todo_app/settings.py
+++ todo_app/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -60,5 +61,6 @@ TIME_ZONE = 'Asia/Seoul'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
